686b515daa4e4d9d7dbde6940668954e0d37c0fe
Jonah Egashira <jonahega@gmail.com>
2020-07-03 10:58:12 +0900

    AC

--- abc165e.cpp
+++ abc165e.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-using ll = long long;
+#define int long long
 
-int main() {
+signed main() {
   int n,m;
   cin >> n >> m;
 
-  if (n % 2 == 1) {
-    for (int l = 2, r = n; l <= m + 1; l++, r--) {
-      cout << l << " " << r << '\n';
+  if(n % 2 == 1) {
+    for (int i = 1; i <= m; i++) {
+      cout << i << " " << n - i + 1 << '\n';
     }
   } else {
-    bool flag = true;
-    for (int l = 2, r = n; l <= m + 1; l++, r--) {
-      if(flag && (r - l) <= n / 2) --r, flag = false;
-      cout << l << " " << r << '\n';
+    int add = 0;
+    for (int i = 1; i <= m; i++) {
+      if((n - i + 1) - i <= n/2) add = -1;
+      cout << i << " " << n - i + 1 + add << '\n';
     }
   }
-
-  return 0;
 }
