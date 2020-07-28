d20f623a730ff012c5dd77fa8cb7f9f113d3e7d8
Jonah Egashira <jonahega@gmail.com>
2020-07-28 18:43:34 +0900

    AC

---
+++ mitsui2019a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)n; i++)
+typedef long long ll;
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int m1,d1,m2,d2;
+  cin >> m1 >> d1 >> m2 >> d2;
+
+  if(m1!=m2) cout << 1 << endl;
+  else cout << 0 << endl;
+
+}
